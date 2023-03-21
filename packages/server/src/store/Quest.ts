ba2d803d7fd942f87bd70e54e70aa3897a3d1faa
root <root@NB-00248.vier.group>
2023-03-21 11:39:30 +0100

    Added store classes

---
+++ packages/server/src/store/Quest.ts
@@ -0,0 +1,44 @@
+export class QuestStore {
+    quests: any[] = [];
+    activeQuest: { id: any; } = null;
+
+    addQuest = (quest: { name: string; description: string; id?: number; }) => {
+        this.quests.push(quest);
+    }
+
+    deleteQuest = (id: number) => {
+        this.quests = this.quests.filter((quest) => {
+            return quest.id !== id
+        });
+
+        if (this?.activeQuest?.id === id) {
+            this.setActiveQuest(null);
+        }
+    }
+
+    editQuest = (id: number, newQuest: { id: any; name?: string; description?: string; }) => {
+        this.quests = this.quests.map((quest) => {
+            if(quest.id !== id){
+                return quest;
+            }
+            return newQuest;
+        });
+
+        if (this?.activeQuest?.id === newQuest.id) {
+            this.setActiveQuest(newQuest.id);
+        }
+    }
+
+    setActiveQuest = (id: number) => {
+        if (id === null){
+            this.activeQuest = null;
+            return;
+        }
+        const newActiveQuest = this.quests.find((quest) => {
+            return quest.id === id
+        });
+        if (newActiveQuest !== undefined){
+            this.activeQuest = newActiveQuest
+        }
+    }
+}
---
+++ packages/server/src/store/Quest2.ts
@@ -0,0 +1,35 @@
+export class QuestStore {
+    _quests = new Map();
+    _activeId: any = null;
+
+    get activeQuest() {
+        return this._quests.get(this._activeId) || null;
+    }
+
+    get quests() {
+        return Array.from(this._quests.values())
+    }
+
+    addQuest = (quest: { name?: string; description?: string; id?: any; }) => {
+        this._quests.set(quest.id, quest);
+    }
+
+    deleteQuest = (id: number) => {
+        this._quests.delete(id);
+    }
+
+    editQuest = (id: number, newQuest: { id: any; name?: string; description?: string; }) => {
+        if(!this._quests.has(id)) {
+            return;
+        }
+        this._quests.set(id, newQuest);
+
+        if (this?.activeQuest?.id === newQuest.id) {
+            this.setActiveQuest(newQuest.id);
+        }
+    }
+
+    setActiveQuest = (id: number) => {
+        this._activeId = id;
+    }
+}
